d128b2d8064058826d5a298d1b83e7d2e424dc48
Ahmed ALnughmush <ahmedwaleedsa1@gmail.com>
2026-01-22 17:14:19 +0300

    Word Counter Utility

---
+++ Lab7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lab7 {
+
+    
+    public static void main(String[] args) {
+        Scanner Ahmed = new Scanner(System.in);
+        String text;
+        int count;
+        
+        System.out.print("Enter a string :");
+        text=Ahmed.nextLine();
+        
+        String[] words = text.trim().split("\\s+");
+        count=words.length;
+        System.out.println("Number of words : "+count);
+        
+        
+        
+    }
+    
+}
