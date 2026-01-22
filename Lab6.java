3e3bdd68481d1ecfef7dc029fe1d6620a916e791
Ahmed ALnughmush <ahmedwaleedsa1@gmail.com>
2026-01-22 14:07:14 +0300

    Character Specific Remover

---
+++ Lab6.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Lab6 {
+ 
+    public static void main(String[] args) {
+        Scanner Ahmed=new Scanner(System.in);
+        String str;
+        String result;
+        int count;
+        char ch;
+        int i ;        
+        
+        System.out.print("Enter a string: ");
+        str=Ahmed.nextLine();
+        
+        
+        result="";
+        count=0;
+      
+        
+        for( i=0 ; i < str.length(); i++ ){
+            ch=str.charAt(i);
+            if((ch=='m'|| ch=='M')&& count<3){
+             count++;
+             continue;
+            }
+            result +=ch;
+        }
+        System.out.println("The new string is :"+result);
+    }
+    
+}
